359a4589d91b98878b7242a0b803c08f94e29719
root <root@DESKTOP-532BVPG.localdomain>
2024-09-28 15:07:52 +0100

    hh

---
+++ ft_strlen.c
@@ -0,0 +1,37 @@
+#include "libft.h"
+
+size_t ft_strlen(const char *str)
+{
+	size_t len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return len;
+}
+
+                                    
+	                     
+	                                   
+			                
+				     
+		                  
+ 
+
+                                  
+ 
+                
+                          
+	 
+	           
+	 
+	              
+ 
+
+	         
+	   
+
+ 
+
+
+
